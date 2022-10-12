369184c04cb05f9dfaa6f22c47fd0aabd8e8de3f
IZMarshmelo <zd16@bk.ru>
2022-10-12 11:27:55 +0300

    New

--- PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/util/Util.java
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,9 +1,12 @@
 package jm.task.core.jdbc.util;
 
 import jm.task.core.jdbc.model.User;
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -27,29 +30,39 @@ public class Util {
     }
 
     public class HibernateUtil {
-        private static SessionFactory concreteSessionFactory;
+        private static SessionFactory sessionFactory;
 
-        static {
-            try {
-                Properties setting = new Properties();
-                setting.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/usersbase2");
-                setting.setProperty("hibernate.connection.username", "root");
-                setting.setProperty("hibernate.connection.password", "root");
-                setting.setProperty("dialect", "org.hibernate.dialect.MySQLDialect");
+        public static SessionFactory getSessionFactory() {
+            if (sessionFactory == null) {
+                try {
+                    Configuration configuration = new Configuration();
 
-                setting.setProperty("hibernate.hbm2ddl.auto", "create");
+                    Properties settings = new Properties();
+                    settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                    settings.put(Environment.URL, URL);
+                    settings.put(Environment.USER, USERNAME);
+                    settings.put(Environment.PASS, PASSWORD);
+                    settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
 
-                concreteSessionFactory = new org.hibernate.cfg.Configuration()
-                        .addProperties(setting)
-                        .addAnnotatedClass(User.class)
-                        .buildSessionFactory();
-            } catch (Exception ex) {
-                throw new ExceptionInInitializerError(ex);
+                    settings.put(Environment.SHOW_SQL, "true");
+
+                    settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                    settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+                    configuration.setProperties(settings);
+
+                    configuration.addAnnotatedClass(User.class);
+
+                    ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                            .applySettings(configuration.getProperties()).build();
+
+                    sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
-        }
-
-        public static Session getSession() throws HibernateException {
-            return concreteSessionFactory.openSession();
+            return sessionFactory;
         }
     }
 }
